582233adf7f085fcdd392655db84a1dc529a121b
opilane <opilane@HP-CZC6297B8B>
2019-05-23 16:38:25 +0300

    Loo klass Kangelane ja teosta nõutavad meetodid.

---
+++ Kangelased/Kangelased/Kangelane.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kangelased
+{
+    class Kangelane
+    {
+        private string _Nimi;
+        private string _Asukoht;
+
+        public Kangelane(string Kangelase_nimi, string Kangelase_asukoht)
+        {
+            Nimi = Kangelase_nimi;
+            Asukoht = Kangelase_asukoht;
+        }
+
+        public string Asukoht { get => _Asukoht; set => _Asukoht = value; }
+        public string Nimi { get => _Nimi; set => _Nimi = value; }
+
+        public int Päästa (int Ohustatuid)
+        {
+            return (int)Math.Round(Ohustatuid * 0.95, 0);
+        }
+
+        public override string ToString()
+        {
+            return Nimi + " kaitseb " + Asukoht;
+        }
+    }
+}
