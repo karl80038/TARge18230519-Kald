eb3b6c23f2bcfc19b5d235714c353f82d7c543fe
opilane <opilane@HP-CZC6297B8B>
2019-05-23 17:36:32 +0300

    Loo klass Superkangelane ning teosta nõutavad meetodid.

---
+++ Kangelased/Kangelased/Superkangelane.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kangelased
+{
+    class Superkangelane : Kangelane
+    {
+        private double _Osavus;
+        public Superkangelane(string Nimi, string Asukoht) : base(Nimi, Asukoht) 
+        {
+            _Osavus = SingleRandom.Instance.Next(1, 60) / 10.0;
+        }
+
+        public override int Päästa(int Ohustatuid)
+        {
+            return (int)Math.Round((95 + _Osavus) / 100 * Ohustatuid, 0);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " ta on selles " + _Osavus + "protsenti parem kui lihtsalt kangelased";
+        }
+    }
+}
